c8ffe8e08051e0d7e680f38f3c4d6ecb87be35f5
mboukhal <lios80466@gmail.com>
2022-09-27 15:41:56 +0100

    Tue Sep 27 14:29:03 +01 2022

--- check.sh
+++ check.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
+function itemInList {
+
+	local list="$1"
+	local item="$2"
+
+	if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]]
+	then
+		# yes, list include item
+		result=0
+	else
+		result=1
+	fi
+	# echo "$list _ $item _ $result" 
+	return $result
+}
+
 function check(){
 
-	by=$(sed -n 6p $1 | awk '{print $3}')
-	mail=$(sed -n 6p $1 | awk '{print $4}' | tr '<' ' ' | tr '@' ' ' | awk '{print $1}')
-	Created=$(sed -n 8p $1 | awk '{print $6}')
-	Updated=$(sed -n 9p $1 | awk '{print $6}')
+	local path="$1"
+	local list=$(echo $2 | tr ',' ' ')
+
+	by=$(sed -n 6p $path | awk '{print $3}')
+	mail=$(sed -n 6p $path | awk '{print $4}' | tr '<' ' ' | tr '@' ' ' | awk '{print $1}')
+	Created=$(sed -n 8p $path | awk '{print $6}')
+	Updated=$(sed -n 9p $path | awk '{print $6}')
+	itemInList "$list" "$by"
+	b=$?
+	itemInList "$list" "$mail"
+	m=$?
+	itemInList "$list" "$Created"
+	c=$?
+	itemInList "$list" "$Updated"
+	u=$?
+
+	((b)) && result="by: $by\n"
+	((m)) && result+="mail: $mail\n"
+	((c)) && result+="Created: $Created\n"
+	((u)) && result+="Updated: $Updated\n"
+
+	printf "$path"
+	(($result == "0")) && printf "\e[32m [OK]\e[0m\n" || printf "\e[31m$result\e[0m\n"
+	# echo $x
+	# exit
 	# echo $by
 	# echo $mail
 	# echo $Created
 	# echo $Updated
-	if [ "$by" = "$USER" ]; then
-		printf "\e[32m$USER\e[0m\n"
-	else
-		printf "\e[31m$by\e[0m\n"
-	fi
+	# if [ "$by" = "$USER" ]; then
+	# 	printf "\e[32m $path: $USER\e[0m\n"
+	# else
+	# 	printf "\e[31m $path: $by\e[0m\n"
+	# fi
 	# USER=$(sed -n -e 6p -e 8,9p $1) #| awk '{print $2, $3, $4, $5, $6}'
 	# USER=$(sed -n -e 6p -e 8,9p $1) #| awk '{print $2, $3, $4, $5, $6}'
 }
@@ -22,25 +59,24 @@ function check(){
 echo -n "Enter the project directory, default [.]: "
 
 read DIR
-echo -n "Enter users list: ${USER}, "
-read UL
 
 [ -z "$DIR" ] && DIR=$(dirname -- "$0")
-[ -z "$UL" ] && UL=$USER
 
-echo "--> $UL"
-exit()
-FILELIST=$(find ${DIR} -type f | grep -e '\.c' -e '\.cpp' -e '\.h' -e '\.hpp') 
+UL=$USER
+echo -n "Enter developers list: ${UL},"
+read TT
+
+UL+=",${TT}"
+
+
+FILELIST=$(find ${DIR} -type f | grep -e '\.c$' -e '\.cpp$' -e '\.h$' -e '\.hpp$') 
 
 i=1
 
 FILECOUNT=$(echo $FILELIST | tr ' ' '\n' | wc -l)
 
-echo 
-
 until [ $i -gt $FILECOUNT ]
 do
-	echo $FILELIST | cut -d " " -f $i
-	check $(echo $FILELIST | cut -d " " -f $i)
+	check $(echo $FILELIST | cut -d " " -f $i) $UL
 	((i=i+1))
 done
