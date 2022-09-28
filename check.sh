831979227d459e2854b1d0d8f4eaae8692e37526
mboukhal <lios80466@gmail.com>
2022-09-28 12:39:20 +0100

    Wed Sep 28 11:53:08 +01 2022

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 function itemInList {
 
 	local list="$1"
@@ -8,12 +9,10 @@ function itemInList {
 	if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]]
 	then
 		# yes, list include item
-		result=0
+		return 0
 	else
-		result=1
+		return 1
 	fi
-	# echo "$list _ $item _ $result" 
-	return $result
 }
 
 function check(){
@@ -25,6 +24,9 @@ function check(){
 	mail=$(sed -n 6p $path | awk '{print $4}' | tr '<' ' ' | tr '@' ' ' | awk '{print $1}')
 	Created=$(sed -n 8p $path | awk '{print $6}')
 	Updated=$(sed -n 9p $path | awk '{print $6}')
+
+	[[ -z "$by" || -z "$mail" || -z "$Created" || -z "$Updated" ]] && out+="\e[33m$path: [KO] No header!\n$result\e[0m\n" && return 1
+
 	itemInList "$list" "$by"
 	b=$?
 	itemInList "$list" "$mail"
@@ -34,27 +36,23 @@ function check(){
 	itemInList "$list" "$Updated"
 	u=$?
 
+	result=""
+	# printf "\n$b\t$m\t$c\t$u\n"
 	((b)) && result="by: $by\n"
 	((m)) && result+="mail: $mail\n"
 	((c)) && result+="Created: $Created\n"
 	((u)) && result+="Updated: $Updated\n"
 
-	printf "$path"
-	(($result == "0")) && printf "\e[32m [OK]\e[0m\n" || printf "\e[31m$result\e[0m\n"
-	# echo $x
+	[[ -z "$result" ]] && printf "\e[32m$path: [OK]\e[0m\n" || out+="\e[31m$path: [KO]\n$result\e[0m\n"
+	# echo $result
 	# exit
 	# echo $by
 	# echo $mail
 	# echo $Created
 	# echo $Updated
-	# if [ "$by" = "$USER" ]; then
-	# 	printf "\e[32m $path: $USER\e[0m\n"
-	# else
-	# 	printf "\e[31m $path: $by\e[0m\n"
-	# fi
-	# USER=$(sed -n -e 6p -e 8,9p $1) #| awk '{print $2, $3, $4, $5, $6}'
-	# USER=$(sed -n -e 6p -e 8,9p $1) #| awk '{print $2, $3, $4, $5, $6}'
 }
+
+out=""
 
 echo -n "Enter the project directory, default [.]: "
 
@@ -65,8 +63,8 @@ read DIR
 UL=$USER
 echo -n "Enter developers list: ${UL},"
 read TT
-
-UL+=",${TT}"
+TT=$(echo $TT | tr ' ' ',')
+UL+=",marvin,${TT}"
 
 
 FILELIST=$(find ${DIR} -type f | grep -e '\.c$' -e '\.cpp$' -e '\.h$' -e '\.hpp$') 
@@ -80,3 +78,5 @@ do
 	check $(echo $FILELIST | cut -d " " -f $i) $UL
 	((i=i+1))
 done
+
+printf "$out"
